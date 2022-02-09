a96a5c798092072b38bc6c73d73175e58cb069de
Rich Briere <rich.briere@gmail.com>
2022-02-09 12:52:00 -0500

    send json test
    build from pug

--- js/scriptscustom.js
+++ js/scriptscustom.js
@@ -3,9 +3,30 @@
 // 
 
 function testReturn() {
-    return "fromFunction";
+    return "testReturn  " + (new Date()) + "<p>";
 }
 
 function decode(value) {
    return value.replace('*', '@').replace(/~/g, '.').split("").reverse().join("");
+}
+
+function loadJSONLocalFile(f, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', f, true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function initJsonGeoCodes() {
+    loadJSONLocalFile('assets/resttest.json', function (response) {
+    // Parse JSON string into object
+    // var actual_JSON = JSON.parse(response);
+    document.write(response);
+});
 }
